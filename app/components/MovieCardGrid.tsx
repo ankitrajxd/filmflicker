260c3e63942e3d4a85b6c76aa575d27682904244
Ankit raj <ankitrajxd@outlook.com>
2024-01-17 17:52:44 +0530

    add release date

--- app/components/MovieCardGrid.tsx
+++ app/components/MovieCardGrid.tsx
@@ -1,50 +1,54 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react'
-import { Movie } from '../services/fetchMovies'
-import AnimatedDiv from './AnimatedDiv';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
-import { Badge } from '@/components/ui/badge';
+import React from "react";
+import { Movie } from "../services/fetchMovies";
+import AnimatedDiv from "./AnimatedDiv";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Badge } from "@/components/ui/badge";
 
 interface Props {
-    Movies: Movie[];
+  Movies: Movie[];
 }
 
-
-const MovieCardGrid = ({Movies}: Props) => {
+const MovieCardGrid = ({ Movies }: Props) => {
   return (
     <div className="columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-4 mt-3">
-        {Movies?.map((movie) => (
-          <AnimatedDiv className="break-inside-avoid" key={movie.id}>
-            <Card className="mb-4 border-2">
-              <CardHeader style={{ overflow: "hidden" }}>
-                <img
-                  src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                  alt="anime-img"
-                  style={{
-                    borderRadius: "6px",
-                    objectFit: "cover",
-                    height: "290px",
-                  }}
-                ></img>
-              </CardHeader>
-              <CardContent className="mt-2">
-                <CardTitle className="text-base mb-1">{movie.title}</CardTitle>
-                <div className="flex justify-between items-center">
-                  <Button className="border-2" size="sm" variant="outline">
-                    <Link href={`/movies/${movie.id}`}>Read More</Link>
-                  </Button>
-                  <Badge variant="outline">
-                    ❤️ {movie.vote_average.toFixed(1)}
-                  </Badge>
-                </div>
-              </CardContent>
-            </Card>
-          </AnimatedDiv>
-        ))}
-      </div>
-  )
-}
+      {Movies?.map((movie) => (
+        <AnimatedDiv className="break-inside-avoid" key={movie.id}>
+          <Card className="mb-4 border-2">
+            <CardHeader style={{ overflow: "hidden" }}>
+              <img
+                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                alt="anime-img"
+                style={{
+                  borderRadius: "6px",
+                  objectFit: "cover",
+                  height: "290px",
+                }}
+              ></img>
+            </CardHeader>
+            <CardContent className="mt-2">
+              <CardTitle className="text-base mb-1">
+                {movie.title}
+                <Badge className="mx-2" variant="outline">
+                  {movie.release_date.split("-")[0]}
+                </Badge>
+              </CardTitle>
+              <div className="flex justify-between items-center">
+                <Button className="border-2" size="sm" variant="outline">
+                  <Link href={`/movies/${movie.id}`}>Read More</Link>
+                </Button>
+                <Badge variant="outline">
+                  ❤️ {movie.vote_average.toFixed(1)}
+                </Badge>
+              </div>
+            </CardContent>
+          </Card>
+        </AnimatedDiv>
+      ))}
+    </div>
+  );
+};
 
-export default MovieCardGrid
+export default MovieCardGrid;
