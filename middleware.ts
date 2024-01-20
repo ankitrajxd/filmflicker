b10ac334722b2d90b57500a018f0773756315e37
Ankit raj <ankitrajxd@outlook.com>
2024-01-20 12:40:46 +0530

    fixed middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,15 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/", "/movies/[id]", "/anime"],
+  publicRoutes: [
+    "/",
+    "/movies/:id+",
+    "/anime",
+    "/movies/lists/nowplaying",
+    "/movies/lists/popular",
+    "/movies/lists/top",
+    "/movies/lists/upcoming",
+  ],
 });
 
 export const config = {
