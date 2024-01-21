489f52d2034d3f8e5b8ce779833b67246f4a9c23
Δnkit-Raj <isankitraj@outlook.com>
2024-01-21 03:40:15 +0000

    fixed signup page

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,10 +1,10 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignUp } from "@clerk/nextjs";
 import React from "react";
 
 const SignInPage = () => {
   return (
     <div className="flex justify-center h-[80vh] items-center p-[1rem] overflow-hidden">
-      <SignIn />
+      <SignUp />
     </div>
   );
 };
