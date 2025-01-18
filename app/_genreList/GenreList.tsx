c7cc708a33cb6d7986d8d38201940f350e1af3d3
Ankit <ankitrajxd@gmail.com>
2025-01-19 00:34:02 +0530

    made sidebar sticky

--- app/_genreList/GenreList.tsx
+++ app/_genreList/GenreList.tsx
@@ -9,10 +9,10 @@ const GenreList = async ({ selectedGenre }: Props) => {
   const genres = await fetchGenre();
 
   return (
-    <>
+    <div className="md:flex md:max-w-[120px] md:flex-col sm:gap-2 hidden my-3 sticky top-5 z-10">
       <h2 className="mb-3 text-2xl ">Genres</h2>
       <List selectedGenre={selectedGenre} data={genres} />
-    </>
+    </div>
   );
 };
 
